23c9216a42e054c532e982529a44b58cac633f9a
RdotA <71939466+IRdotAI@users.noreply.github.com>
2025-10-04 21:38:25 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -76,7 +76,14 @@ if game.PlaceId == 85896571713843 then
         end
     end
 
-    --Auro Hatch Function
+    function AutoHatch()
+        while _G.AutoHatch do
+            if _G.SelectedEgg ~= "" then
+                game:GetService("ReplicatedStorage").Shared.Framework.Network.Remote.RemoteEvent:FireServer("HatchEgg", _G.SelectedEgg,6)
+            end
+            task.wait(0.3)
+        end
+    end
     
 
 
