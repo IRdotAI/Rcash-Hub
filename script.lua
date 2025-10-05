df419f4bc4e040fb9bed28e6ee5367db3682ccc0
RdotA <71939466+IRdotAI@users.noreply.github.com>
2025-10-05 16:09:57 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/IRdotAI/Rcash-Hub/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/IRdotAI/Rcash-Hub/refs/heads/main/main.lua"))()
