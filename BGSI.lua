8833038086502c7383bfee7f187c7ac9c12b9c81
RdotA <71939466+IRdotAI@users.noreply.github.com>
2025-10-08 09:22:18 +0100

    Update BGSI.lua

--- BGSI.lua
+++ BGSI.lua
@@ -1,6 +1,12 @@
 if game.PlaceId == 85896571713843 then
 -- UI Library (Fluent-Renewed)
     local Library = loadstring(game:HttpGetAsync("https://github.com/ActualMasterOogway/Fluent-Renewed/releases/latest/download/Fluent.luau"))()
+
+    if not Library then
+        warn("Rcash Hub: FAILED to load Fluent UI Library. Script execution halted.")
+        return -- Stop the script from running further
+    end
+
     local SaveManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/SaveManager.luau"))()
     local InterfaceManager = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/ActualMasterOogway/Fluent-Renewed/master/Addons/InterfaceManager.luau"))()
 
