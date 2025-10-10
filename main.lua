d69e2805c1fe0efe0264f3a8a6dd97d1677b18b7
RdotA <71939466+IRdotAI@users.noreply.github.com>
2025-10-10 13:58:34 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ local games = {
     [13667319624] = "https://raw.githubusercontent.com/IRdotAI/Rcash-Hub/refs/heads/main/KS.lua",
     [85896571713843] = "https://raw.githubusercontent.com/IRdotAI/Rcash-Hub/refs/heads/main/BGSI.lua",
     [286090429] = "https://raw.githubusercontent.com/IRdotAI/Rcash-Hub/refs/heads/main/ARS.lua",
+	[128886701132186] = ""
 }
 
 local scriptURL = games[game.PlaceId]
@@ -14,6 +15,7 @@ else
 	    Icon = "rbxassetid://110392278560658"
     })
 end
+
 
 
 
